3f98428eff9cfd051fee367e7a905c513fa93ca3
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-09-25 21:43:39 -0400

    These are my codewars solutions - 8kyu

---
+++ ConvertNumber.js
@@ -0,0 +1,6 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+  return num.toString() 
+  }
---
+++ OppositeNumber.js
@@ -0,0 +1,6 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+    //your code here
+    return -number 
+  }
