57c1b558d5a900ced7eba8dcee50496b18e93a94
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-09-27 10:24:11 -0400

    New Code Wars

---
+++ ReturnNegative.js
@@ -0,0 +1,12 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+    // Code?
+   if(num > 0) {
+     num = num * -1
+     return num
+    }else {
+      return num
+    }
+  }
+  
