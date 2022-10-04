5a75adfafebdbaa3d49f6232347000b1f04f6924
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-10-03 23:06:23 -0400

    New CodeWars Solution

---
+++ CenturyFromYear.js
@@ -0,0 +1,8 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year/100);
+  }
+
+  
