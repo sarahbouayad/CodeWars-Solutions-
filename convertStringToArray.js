2ae6bcd2593396ea20a65c9cda8bb89ab87789d7
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-10-31 18:06:12 -0400

    new

---
+++ convertStringToArray.js
@@ -0,0 +1,10 @@
+// Write a function to split a string and convert it into an array of words.
+
+
+
+
+function stringToArray(string){
+    let newArray = string.split(" ")
+    return newArray
+        // code code code
+    }
