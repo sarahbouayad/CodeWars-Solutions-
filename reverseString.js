784cfabc141e79f92388478f058c8866da47d7ad
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-11-03 18:57:33 -0400

    push

---
+++ reverseString.js
@@ -0,0 +1,14 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+
+function reverseWords(str) {
+    // Go for it
+    
+   return str.split('').reverse().join('').split(' ').reverse().join(' ')
+    
+  //  let newString = str.split(" ")
+  //  console.log(newString)
+   
+  //  newString.reverse().join.forEach(word => newString.split().reverse().join(' '))
+   
+  }
