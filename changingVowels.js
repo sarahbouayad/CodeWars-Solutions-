5b7aaf82fdf582124e75167dfea021b2fb54c263
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-11-07 19:53:15 -0500

    code war

---
+++ changingVowels.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+const swap = string => 
+// use replace as function
+// / [] / regular expression, g is global
+// toUpperCase to capitalize letters
+// element indiciating elements of string. 
+string.replace(/[a,e,i,o,u]/g, element => element.toUpperCase())
