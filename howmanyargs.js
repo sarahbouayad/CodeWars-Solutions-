cf2d4cf28ac0e33e06ce631fb4da0b814b100022
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-12-05 19:15:44 -0500

    how many args

---
+++ howmanyargs.js
@@ -0,0 +1,8 @@
+// Create a function called args_count that returns the number of arguments provided
+// function args_count 
+// count by index 
+// return the number of args provided 
+
+const args_count = (...args) => args.length
+
+
