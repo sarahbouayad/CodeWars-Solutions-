3874e91b518d41fafd7bfb685d0015f3910c0f0e
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-12-15 19:24:02 -0500

    new

---
+++ beginnerseries4.js
@@ -0,0 +1,13 @@
+function cockroachSpeed(s) {
+    //Good Luck!
+    console.log(s)
+  // write a function that takes its speed in km per hour, returns in cm per second, 
+  // rounded to the integer (= floored)
+  //   P: speed in km per hour
+  //   R: return cm per second, rounded down to the integer 
+  //   E: 1.08 ---> 30 
+  
+  return Math.floor(s * 27.777778)
+  }
+
+  
