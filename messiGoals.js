2f022076e3d7bd2aec44c0f9cb5e433da2ac44f3
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-12-19 17:17:30 -0500

    new

---
+++ messiGoals.js
@@ -0,0 +1,7 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
