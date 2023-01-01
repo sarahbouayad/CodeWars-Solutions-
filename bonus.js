a9262ba6c85ae421b36e25e08253c5d192ab0d14
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2023-01-01 11:16:08 -0500

    new

---
+++ bonus.js
@@ -0,0 +1,9 @@
+function bonusTime(salary, bonus) {
+    // your code here
+      if (bonus === true){
+        return "£" + (salary * 10);
+      }
+      else {
+        return "£" + salary
+      }
+    }
