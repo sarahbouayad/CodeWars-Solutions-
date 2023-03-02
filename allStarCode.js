6262f76c0f165d9e5f8df50bdabf7db90d99ff40
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2023-03-02 14:34:57 -0500

    new solution

--- allStarCode.js
+++ allStarCode.js
@@ -1,3 +1,7 @@
 function strCount(str, letter){  
     return str.split(letter).length-1
   }
+
+  function strCount(str, letter){
+    return String.split(letter).length-1
+  }
---
+++ solutionsJS.js
@@ -0,0 +1,8 @@
+function greet(name,owner){
+
+    if(name == owner){
+        return `Hello ${name}`
+    }else{
+        return `Goodbye ${owner}`
+    }
+}
